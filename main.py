0e1b4441aa29892b46c3d24dfcfa936a720c845e
Khushi <khushirajpurohit617@gmail.com>
2025-02-13 14:26:55 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from src.text_summarizer.logging.logger import logging
 
 from src.text_summarizer.config.configuration import ConfigurationManager
 from src.text_summarizer.components.data_ingestion import DataIngestion
+from src.text_summarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-config=ConfigurationManager()
-data_ingestion_config=config.get_data_ingestion_config()
-data_ingestion=DataIngestion(config=data_ingestion_config)
 
-data_ingestion.download_file()
-data_ingestion.extract_zip_file()
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+    logging.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logging.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logging.exception(e)
+    raise e
